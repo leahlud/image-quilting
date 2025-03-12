131c70a4ff8efdf7c132295dd1456c5c9d7c559d
Leah Ludwikowski <leahlud11@outlook.com>
2025-03-12 16:59:03 -0500

    Added lab 5

---
+++ lab-5-microphone-arrays/README.md
@@ -0,0 +1,7 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/l485ogIW)
+# Lab 5: Microphone Arrays
+In this lab we will perform some simple microphone array processing. We will use the sound below:
+
+[https://drive.google.com/uc?export=download&id=1emuGR4tlmemJ8RXSD1rWQwNx13h5X9VM ]
+
+This is a recording from an 8-channel array. The microphones were placed at a distance of 0.1 meters from each other, and two simultaneous sounding sources were recorded. In the rest of this lab you will have to find where the sources are, and beamform so that you focus on each one separately.
---
+++ lab-5-microphone-arrays/utils.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load a remote WAVE file given its URL, and return the sample rate and waveform
+def wavreadurl( url):
+    import urllib.request, io, scipy.io.wavfile
+    f = urllib.request.urlopen( urllib.request.Request( url))
+    sr,s = scipy.io.wavfile.read( io.BytesIO( f.read()))
+    return sr, s.astype( 'float32')/32768
+
+# Load a local WAVE file given its filepath, and return the sample rate and waveform
+def wavreadfile(filename):
+    import scipy.io.wavfile
+    sr, s =  scipy.io.wavfile.read(filename)
+    return sr, s.astype( 'float32')/32768
+
+# Make a sound player function that plays array "x" with a sample rate "rate", and labels it with "label"
+def sound( x, rate=8000, label=''):
+    from IPython.display import display, Audio, HTML
+    display( HTML( 
+    '<style> table, th, td {border: 0px; }</style> <table><tr><td>' + label + 
+    '</td><td>' + Audio( x, rate=rate)._repr_html_()[3:] + '</td></tr></table>'
+    ))
+
+# Compute number of frames for STFT based on padding
+def get_num_frames(input_sound, dft_size, hop_size):
+    n = len(input_sound)
+    hop_remainder = (n - dft_size) % hop_size
+    if hop_remainder != 0:
+        input_sound = np.append(input_sound, np.zeros(hop_remainder))
+        n += hop_remainder
+    num_frames = ((n - dft_size) // hop_size) + 1
+    return num_frames
+
+# Convert input sound to time/frequency domain for spectrogram plotting
+def stft(input_sound, dft_size, hop_size, window):
+    # get windowed segments of input sound and compute DFT of each
+    spectrogram = []
+    n = len(input_sound)
+
+    for i in range(0, n - dft_size - 1, hop_size):
+        #  get frame, padding with zeros if necessary
+        frame = []
+        if i + dft_size < n:
+            frame = input_sound[i:i+dft_size]
+        else:
+            frame = np.append(input_sound[i:n], np.zeros((i + dft_size) - n))
+
+        # compute DFT
+        frame_dft = np.fft.rfft(frame * window)
+
+        # append to spectrogram array
+        spectrogram.append(frame_dft) # append as rows
+
+    # Return a complex-valued spectrogram (frequencies x time)
+    return np.array(spectrogram).T
+
+# Convert time/frequency domain sound back into time-domain waveform
+def istft(stft_output, dft_size, hop_size, window):
+    num_frames = np.shape(stft_output)[1]
+    n = dft_size + ((num_frames - 1) * hop_size) 
+    output_sound = np.zeros(n)
+
+    for i in range(num_frames):
+        # compute inverse FFT for current frame
+        frame_idft = np.fft.irfft(stft_output[:,i], n=dft_size)
+        frame = frame_idft * np.sqrt(window)
+
+        # add result to output_sound at correct indices
+        start_idx = i * hop_size
+        output_sound[start_idx : start_idx + dft_size] += frame
+    
+    # Return reconstructed waveform
+    return output_sound
+    
+# Plot the given spectrogram using numpy.pcolormesh
+def plot_spectrogram(spectrogram, dft_size, hop_size, sample_rate, label, cmap="viridis"):
+    # calculate the time axis labels
+    num_frames = np.shape(spectrogram)[1]
+    samples = (num_frames * hop_size) + dft_size
+    times = np.round(np.linspace(0, samples / sample_rate, num=5), 2)
+
+    # calculate the frequency axis labels
+    max_freq = sample_rate / 2
+    freqs = np.linspace(0, sample_rate / 2, num=6)
+
+    # plot the spectrogram
+    plt.pcolormesh(np.abs(spectrogram)**0.2, cmap=cmap) # np.abs gives magnitude of complex number
+    plt.xticks(ticks=np.linspace(0, spectrogram.shape[1], len(times)), labels=times)
+    plt.yticks(np.linspace(0, spectrogram.shape[0], len(freqs)), labels=freqs)
+    plt.title("Spectrogram for " + label + " Sound")
+    plt.xlabel("Time (seconds)")
+    plt.ylabel("Frequency (Hz)")
